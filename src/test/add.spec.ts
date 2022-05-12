04605c47cb2263c54157e2818cf3b22a2a18e8e5
haechan <kiki9510@gmail.com>
2022-05-13 01:18:10 +0900

    테스트 폴더 및 파일 생성

---
+++ src/test/add.spec.ts
@@ -0,0 +1,11 @@
+import { add } from "../add";
+
+describe("Test add function", () => {
+    it("Add - success", () => {
+        expect(add(2, 3)).toBe(5);
+    });
+
+    // it("Add - fail", () => {
+    //     expect(add(2, 2)).toBe(5)
+    // })
+});
