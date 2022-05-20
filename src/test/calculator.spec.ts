efeaa2b2ed7d9bdf687f03efb2a2dc39f19683b7
haechan <kiki9510@gmail.com>
2022-05-20 21:16:24 +0900

    set 메소드 테스트 코드 추가, beforeEach로 인스턴스 생성

--- src/test/calculator.spec.ts
+++ src/test/calculator.spec.ts
@@ -1,8 +1,17 @@
 import { Calculator } from "../calculator";
 
 describe("Calculator TEST", () => {
+    let cal: { value: number; set: (arg0: number) => void };
+    beforeEach(() => {
+        cal = new Calculator();
+    });
+
     it("init calculator - value 0", () => {
-        const cal = new Calculator();
         expect(cal.value).toBe(0);
+    });
+
+    it("set number", () => {
+        cal.set(3);
+        expect(cal.value).toBe(3);
     });
 });
