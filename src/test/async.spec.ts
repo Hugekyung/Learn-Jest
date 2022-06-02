7cf9b7aaadd71532c1e39b5c8c613cedda778d6f
haechan <kiki9510@gmail.com>
2022-06-03 00:39:00 +0900

    async / await 방식의 비동기처리를 활용한 테스트 코드 작성

--- src/test/async.spec.ts
+++ src/test/async.spec.ts
@@ -1,12 +1,6 @@
 import { fetchProduct } from "../asnyc";
 
 describe("Test for fetchProduct", () => {
-    // it("should be return error message", async () => {
-    //     const error = "error";
-    //     const result = await fetchProduct(error);
-    //     expect(result).toThrow("network error");
-    // });
-
     // * done 방식은 코드가 지저분해지고, 수행 속도도 느려 권장하지 않음
     it("should be return item - done", (done) => {
         fetchProduct().then((item) => {
@@ -19,5 +13,22 @@ describe("Test for fetchProduct", () => {
         return fetchProduct().then((item) => {
             expect(item).toEqual({ item: "Milk", price: 200 });
         });
+    });
+
+    it("should be return item - async / await", async () => {
+        const product = await fetchProduct();
+        expect(product).toEqual({ item: "Milk", price: 200 });
+    });
+
+    it("async - resolves", () => {
+        return expect(fetchProduct()).resolves.toEqual({
+            item: "Milk",
+            price: 200,
+        });
+    });
+
+    it("async - rejects", () => {
+        const error = "error";
+        return expect(fetchProduct(error)).rejects.toBe("network error");
     });
 });
