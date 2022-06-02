5f28e7f96b518a9c80a30e2a885291cf5608e23c
haechan <kiki9510@gmail.com>
2022-06-02 23:13:17 +0900

    나누기 관련 테스트 상황 추가

--- src/test/calculator.spec.ts
+++ src/test/calculator.spec.ts
@@ -21,16 +21,17 @@ describe("Calculator TEST", () => {
         cal.clear();
         expect(cal.value).toBe(0);
     });
+    describe("add", () => {
+        it("add", () => {
+            cal.add(3);
+            cal.add(2);
+            expect(cal.value).toBe(5);
+        });
 
-    it("add", () => {
-        cal.add(3);
-        cal.add(2);
-        expect(cal.value).toBe(5);
-    });
-
-    it("add - limit 100", () => {
-        cal.add(20);
-        expect(() => cal.add(100)).toThrow(Error);
+        it("add - limit 100", () => {
+            cal.add(20);
+            expect(() => cal.add(100)).toThrow(Error);
+        });
     });
 
     it("subtract", () => {
@@ -44,15 +45,28 @@ describe("Calculator TEST", () => {
         expect(cal.value).toBe(4);
     });
 
-    it("divide", () => {
-        cal.add(9);
-        cal.divide(3);
-        expect(cal.value).toBe(3);
-    });
+    describe("divides", () => {
+        it("divide", () => {
+            cal.add(9);
+            cal.divide(3);
+            expect(cal.value).toBe(3);
+        });
 
-    it("divide - float", () => {
-        cal.add(10);
-        cal.divide(3);
-        expect(cal.value).toBeGreaterThan(3);
+        it("divide - float", () => {
+            cal.add(10);
+            cal.divide(3);
+            expect(cal.value).toBeGreaterThan(3);
+        });
+
+        it("0 / 0 === NaN", () => {
+            cal.divide(0);
+            expect(cal.value).toBeNaN();
+        });
+
+        it("1 / 0 === Infinity", () => {
+            cal.set(1);
+            cal.divide(0);
+            expect(cal.value).toBe(Infinity);
+        });
     });
 });
