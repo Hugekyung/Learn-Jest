e877d03f250839b57e0c0984d99fe02c5eeb2878
haechan <kiki9510@gmail.com>
2022-06-24 18:44:42 +0900

    Stack pop() method에 에러핸들링 테스트 추가

--- src/stack/stack.ts
+++ src/stack/stack.ts
@@ -15,4 +15,11 @@ export class Stack {
     push(item: any) {
         this.array.push(item);
     }
+
+    pop() {
+        if (this.array.length === 0) {
+            throw new Error("스택이 비었습니다.");
+        }
+        return this.array.pop();
+    }
 }
--- src/stack/test/stack.test.ts
+++ src/stack/test/stack.test.ts
@@ -16,4 +16,18 @@ describe("Stack TEST", () => {
         expect(stack.size()).toBe(1);
         expect(stack.getStack()).toEqual([2]);
     });
+
+    it("pop() method를 통해 스택에 마지막으로 등록된 아이템을 제거할 수 있고, 그 값을 반환받는다.", () => {
+        stack.push(1);
+        stack.push(2);
+        expect(stack.size()).toBe(2);
+        expect(stack.pop()).toEqual(2);
+        expect(stack.getStack()).toEqual([1]);
+    });
+
+    it("pop() method를 사용했을 때 스택이 비어있다면, 에러를 반환한다.", () => {
+        expect(() => {
+            stack.pop();
+        }).toThrow("스택이 비었습니다.");
+    });
 });
